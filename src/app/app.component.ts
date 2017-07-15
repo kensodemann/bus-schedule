397d0196d32ea580fb00c532a7839e7d7645ce4b
Ken Sodemann <ken.sodemann@gmail.com>
2017-07-15 05:48:29 -0500

    feat(map): add a simple options service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Bus Schedule';
-
-  constructor() {
-  }
+  constructor() {}
 }
