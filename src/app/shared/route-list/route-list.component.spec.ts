67c2793a17d5d681fe48511b6a7cc1d3cf4f59ae
Ken Sodemann <ken.sodemann@gmail.com>
2017-07-17 06:17:54 -0500

    WIP -  test

--- src/app/shared/route-list/route-list.component.spec.ts
+++ src/app/shared/route-list/route-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { MdCheckboxModule, MdListModule } from '@angular/material';
+import { Subject } from 'rxjs/Subject';
 
 import { Route } from '../../core/routes/route';
 import { RouteItemComponent } from './route-item/route-item.component';
@@ -8,8 +9,14 @@ import { RouteListComponent } from './route-list.component';
 import { RouteOptionsService } from '../../core/route-options/route-options.service';
 
 class RouteOptionsServiceMock {
+  changedOptions: Subject<Array<any>>;
+
+  constructor() { this.changedOptions = new Subject();}
+
   hideRoute(agency: string, route: string | Array<string>) { }
   showRoute(agency: string, route: string | Array<string>) { }
+
+  shouldDisplayRoute(agency: string, route: string): boolean { return true; }
 }
 
 describe('RouteListComponent', () => {
@@ -38,6 +45,7 @@ describe('RouteListComponent', () => {
     initiailizeTestData();
     fixture = TestBed.createComponent(RouteListComponent);
     component = fixture.componentInstance;
+    component.routes = testRoutes;
     fixture.detectChanges();
   });
 
@@ -48,11 +56,25 @@ describe('RouteListComponent', () => {
   describe('select all routes', () => {
     describe('true', () => {
       it('registers the change with the route options service', () => {
+        const routeOptions = fixture.debugElement.injector.get(RouteOptionsService);
+        spyOn(routeOptions, 'hideRoute');
+        spyOn(routeOptions, 'showRoute');
+        component.onSelectAllChecked(true);
+        expect(routeOptions.showRoute).toHaveBeenCalledTimes(1);
+        expect(routeOptions.showRoute).toHaveBeenCalledWith('sf-muni', ['1', '1AX', '1BX', '2', '3', '5', '5R']);
+        expect(routeOptions.hideRoute).not.toHaveBeenCalled();
       });
     });
 
     describe('false', () => {
       it('registers the change with the route options service', () => {
+        const routeOptions = fixture.debugElement.injector.get(RouteOptionsService);
+        spyOn(routeOptions, 'hideRoute');
+        spyOn(routeOptions, 'showRoute');
+        component.onSelectAllChecked(true);
+        expect(routeOptions.showRoute).toHaveBeenCalledTimes(1);
+        expect(routeOptions.showRoute).toHaveBeenCalledWith('sf-muni', ['1', '1AX', '1BX', '2', '3', '5', '5R']);
+        expect(routeOptions.hideRoute).not.toHaveBeenCalled();
       });
     });
 
